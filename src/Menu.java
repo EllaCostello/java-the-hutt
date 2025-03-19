9701112ce43fb7ddaea2d191fe28652817bfd159
nikholt1 <140625263+nikholt1@users.noreply.github.com>
2025-03-19 09:31:45 +0100

    Initialize and create Pizza class with list of pizzas and test objects in "menu class" and "Main class"

--- src/Menu.java
+++ src/Menu.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class Menu {
-
-
     // pizzas arraylist
+    Pizza pizza = new Pizza();
+
     // display menu arrayslist pizza
+    public void printMenu() {
+        pizza.showPizzas();
+
+    }
 
 
 
