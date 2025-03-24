f00ed6e6ebb91031f09c6b41642034fddeb667b4
Manden9753 <andr9753@gmail.com>
2025-03-24 21:28:50 +0100

    første udkast til UserInterface

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,8 @@ public class Main {
     public static void main(String[] args) {
 //        Menu menu = new Menu();
 //        menu.printMenu();
-
-        Order order = new Order(30);
-        order.addPizzaToOrder(1);
-        order.addPizzaToOrder(30);
-        // System.out.println(order);
-
-        order.setOrderstatus(OrderStatus.COMPLETED);
-
-        Order order1 = new Order(15);
-        order1.addPizzaToOrder(5);
-        order1.setOrderstatus(OrderStatus.COMPLETED);
-
-        Order order2 = new Order(40);
-        order2.addPizzaToOrder(10);
-        order2.setOrderstatus(OrderStatus.ACTIVE);
-        System.out.println(order2);
-        order2.setOrderstatus(OrderStatus.COMPLETED);
-
-
-        Order.getOrderHistory().getAllOrders();
-        System.out.println(Order.getOrderHistory().returnTurnoverTotal());
-
-        order2.setIngredient(10, "tomato");
-        order2.getIngredient(1);
-        System.out.println(order2.getIngredient(10));
+        Pizza pizza = new Pizza();
+        UserInterface userInterface = new UserInterface(pizza);
+        userInterface.inputHandling();
     }
 }
