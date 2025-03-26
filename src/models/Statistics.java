56ecdb12ea0daf430ad52b54594109de06dd3b6c
CoDIaCx2356 <soderstjernagustav@gmail.com>
2025-03-26 11:16:59 +0100

    Statistics update modified to ensure future product scalability. Statistics class also modified to show top 3 most popular products instead of singular iteration. UserInterface slightly modified to call on Statistics class and show popular function. Additionally also added small "Return to menu" function by pressing Enter.

--- src/models/Statistics.java
+++ src/models/Statistics.java
@@ -22,7 +22,6 @@ public class Statistics {
                 allTimeOrders.add(order);
             }
         }
-
         System.out.println("Total completed orders collected: " + allTimeOrders.size());
     }
 
@@ -32,18 +31,27 @@ public class Statistics {
             return;
         }
 
-        int[] productCount = new int[31];
-
+        // Find highest pizza number
+        int maxProductNumber = 0;
         for (Order order : allTimeOrders) {
             for (Product product : order.getOrderList()) {
-                int productNumber = product.getProductNumber();
-                productCount[productNumber]++;
+                if (product.getProductNumber() > maxProductNumber) {
+                    maxProductNumber = product.getProductNumber();
+                }
             }
         }
 
+        // Count pizza orders
+        int[] productCount = new int[maxProductNumber + 1];
+        for (Order order : allTimeOrders) {
+            for (Product product : order.getOrderList()) {
+                productCount[product.getProductNumber()]++;
+            }
+        }
+
+        // Find most popular
         int maxCount = 0;
         int mostPopularProductNumber = 0;
-
         for (int i = 1; i < productCount.length; i++) {
             if (productCount[i] > maxCount) {
                 maxCount = productCount[i];
@@ -51,10 +59,78 @@ public class Statistics {
             }
         }
 
-        Menu menu = new Menu();
-        String productName = menu.getProducts().get(mostPopularProductNumber - 1).toString();
+        // Get pizza name
+        String productName = "Unknown Product";
+        for (Order order : allTimeOrders) {
+            for (Product product : order.getOrderList()) {
+                if (product.getProductNumber() == mostPopularProductNumber) {
+                    productName = product.toString();
+                    break;
+                }
+            }
+            if (!productName.equals("Unknown Product")) break;
+        }
 
         System.out.println("Mest populære produkt: " + productName);
         System.out.println("Bestilt " + maxCount + " gange");
+    }
+
+    public void printTop3MostPopularPizzas(ArrayList<Order> allTimeOrders) {
+        if (allTimeOrders.isEmpty()) {
+            System.out.println("No orders available to analyze.");
+            return;
+        }
+
+        // Find highest pizza number
+        int maxProductNumber = 0;
+        for (Order order : allTimeOrders) {
+            for (Product product : order.getOrderList()) {
+                if (product.getProductNumber() > maxProductNumber) {
+                    maxProductNumber = product.getProductNumber();
+                }
+            }
+        }
+
+        // Count pizza orders
+        int[] productCount = new int[maxProductNumber + 1];
+        for (Order order : allTimeOrders) {
+            for (Product product : order.getOrderList()) {
+                productCount[product.getProductNumber()]++;
+            }
+        }
+
+        System.out.println("\nTop 3 Most Popular Pizzas:");
+        for (int rank = 1; rank <= 3; rank++) {
+            int maxCount = 0;
+            int popularPizzaNumber = 0;
+
+            // Find current most popular
+            for (int i = 1; i < productCount.length; i++) {
+                if (productCount[i] > maxCount) {
+                    maxCount = productCount[i];
+                    popularPizzaNumber = i;
+                }
+            }
+
+            // Print and exclude from next iterations
+            if (maxCount > 0) {
+                String pizzaName = "Unknown Pizza";
+                for (Order order : allTimeOrders) {
+                    for (Product product : order.getOrderList()) {
+                        if (product.getProductNumber() == popularPizzaNumber) {
+                            pizzaName = product.toString();
+                            break;
+                        }
+                    }
+                    if (!pizzaName.equals("Unknown Pizza")) break;
+                }
+
+                System.out.printf("%d. %s (Ordered %d times)%n",
+                        rank, pizzaName, maxCount);
+                productCount[popularPizzaNumber] = 0;
+            } else {
+                break; // No more pizzas with orders
+            }
+        }
     }
 }
